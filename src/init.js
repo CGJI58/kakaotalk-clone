b86ce63cdfece4e71ae896c1183c57b93793c175
CGJI58 <leemsk22@naver.com>
2022-04-12 17:38:02 +0900

    initial setting(w/o db)

---
+++ src/init.js
@@ -0,0 +1,8 @@
+import app from "./server";
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`✅ Server listening on http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
