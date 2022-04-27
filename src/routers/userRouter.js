6caf9ad7ccd888c13cfba49e7c87791f8fe01730
CGJI58 <leemsk22@naver.com>
2022-04-27 15:40:05 +0900

    Delete some pages

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,8 @@
 import express from "express";
-import { friends, find, more, settings } from "../controllers/userController";
+import { friends } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/", friends);
-userRouter.get("/find", find);
-userRouter.get("/more", more);
-userRouter.get("/settings", settings);
 
 export default userRouter;
